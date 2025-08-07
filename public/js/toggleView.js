b8c8290a9c3b814e959b6a5d4fb315a52470f7c3
andriyivanyuk <andriyivvanyuk@gmail.com>
2025-08-07 15:39:22 +0300

    list added

--- public/js/toggleView.js
+++ public/js/toggleView.js
@@ -1,26 +1,22 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const tabs = document.querySelectorAll(".nav-tabs li a");
-//   const panes = document.querySelectorAll(".tab-content .tab-pane");
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".nav-tabs li a");
+  const panes = document.querySelectorAll(".tab-content .tab-pane");
 
-//   console.log(tabs, panes);
+  console.log(tabs, panes);
 
-//   tabs.forEach((tab) => {
-//     tab.addEventListener("click", (e) => {
-//       e.preventDefault();
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+      e.preventDefault();
 
-//       // Знімаємо active з усіх табів
-//       tabs.forEach((t) => t.classList.remove("active", "show"));
-//       // Ставимо active на обраний таб
-//       tab.classList.add("active", "show");
+      tabs.forEach((t) => t.classList.remove("active", "show"));
+      tab.classList.add("active", "show");
 
-//       // Знімаємо active з усіх блоків з товарами
-//       panes.forEach((pane) => pane.classList.remove("in", "active", "show"));
+      panes.forEach((pane) => pane.classList.remove("in", "active", "show"));
 
-//       // Отримуємо ID таба, наприклад "#grid" або "#list"
-//       const target = document.querySelector(tab.getAttribute("href"));
-//       if (target) {
-//         target.classList.add("in", "active", "show");
-//       }
-//     });
-//   });
-// });
+      const target = document.querySelector(tab.getAttribute("href"));
+      if (target) {
+        target.classList.add("in", "active", "show");
+      }
+    });
+  });
+});
